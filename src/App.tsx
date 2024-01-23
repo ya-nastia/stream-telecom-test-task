f4fe5f5d7dcfc62a374af9e2649988198deba1aa
Nastia <nastiakopylova1@mail.ru>
2024-01-23 17:05:32 +0300

    added Container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import { Container } from './components/Container';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Container>
+      content
+    </Container>
   );
 }
 
---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Container = styled.main`
+  max-width: 600px;
+  margin: 50px auto;
+`;
+
+export default Container;
