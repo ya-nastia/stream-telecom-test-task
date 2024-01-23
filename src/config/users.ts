7248c4eb0a639a0a745ed42293915907866f8dec
Nastia <nastiakopylova1@mail.ru>
2024-01-23 17:00:10 +0300

    added users config

---
+++ src/config/users.ts
@@ -0,0 +1,23 @@
+interface IUser {
+  name: string;
+  email: string;
+  phone: string;
+}
+
+export const users: IUser[] = [
+  {
+    name: 'Иванов Иван Иванович',
+    email: 'ivanov@ivan.ivan',
+    phone: '+70000000000',
+  },
+  {
+    name: 'Петров Петр Петрович',
+    email: 'petrov@petr.petr',
+    phone: '+70000000001',
+  },
+  {
+    name: 'Сидоров Сергей Сергеевич',
+    email: 'sidorov@sidor.sidor',
+    phone: '+70000000002',
+  },
+];
